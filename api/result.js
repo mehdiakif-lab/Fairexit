64ad8ebd0d80816935b57f6603ed43d1330fb7eb
mehdiakif-lab <mehdiakif@gmail.com>
2026-03-09 12:55:30 -0400

    Update result.js

--- api/result.js
+++ api/result.js
@@ -1,1 +1,8 @@
+export default function handler(req, res) {
+  if (!global.analysisResult) {
+    return res.status(404).send("No analysis available");
+  }
+
+  res.status(200).send(global.analysisResult);
+}
 
